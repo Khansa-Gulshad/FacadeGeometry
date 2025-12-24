49c977d67780e35c5c99ea52114fd08f45ddd589
Khansa-Gulshad <64579838+Khansa-Gulshad@users.noreply.github.com>
2025-12-24 14:53:55 +0100

    Update road_network.py

--- SAM_modules/modules/road_network.py
+++ SAM_modules/modules/road_network.py
@@ -1,18 +1,8 @@
-from vt2geojson.tools import vt_bytes_to_geojson
-from shapely.geometry import Point
-from scipy.spatial import cKDTree
 import geopandas as gpd
 import osmnx as ox
-import mercantile
-
-from concurrent.futures import ThreadPoolExecutor, as_completed
-from tqdm import tqdm
-import pandas as pd
-import numpy as np
-import requests
 import math
+from shapely.geometry import Point
 import networkx as nx
-
 
 # Function adapted from iabrilvzqz
 # GitHub: https://github.com/Spatial-Data-Science-and-GEO-AI-Lab/StreetView-NatureVisibility-GSV
@@ -72,7 +62,7 @@ def get_road_network(city, bbox):
     # This function projects the graph to the UTM CRS for the UTM zone in which the graph's centroid lies
     _ , edges = ox.graph_to_gdfs(G_proj) 
 
-    return edges
+    return edges, G_proj
 
 def remove_intersection_points(points, G, min_dist=12.0):
     """
@@ -132,106 +122,10 @@ def select_points_on_road_network(roads, N=50):
 # Function courtesy of iabrilvzqz
 # GitHub: https://github.com/Spatial-Data-Science-and-GEO-AI-Lab/StreetView-NatureVisibility-GSV
 # This function extracts the features for a given tile
-def get_features_for_tile(tile, access_token):
-    # This URL retrieves all the features within the tile. These features are then going to be assigned to each sample point depending on the distance.
-    tile_url = f"https://tiles.mapillary.com/maps/vtp/mly1_public/2/{tile.z}/{tile.x}/{tile.y}?access_token={access_token}"
-    response = requests.get(tile_url)
-    result = vt_bytes_to_geojson(response.content, tile.x, tile.y, tile.z, layer="image")
-    return [tile, result]
-
 
 # Function adapted from iabrilvzqz
 # GitHub: https://github.com/Spatial-Data-Science-and-GEO-AI-Lab/StreetView-NatureVisibility-GSV
-# Creates features from sample points
-def get_features_on_points(points, road, access_token, max_distance=50, zoom=14):
-  # Store the local crs in meters that was assigned by osmnx previously so we can use it to calculate the distances between features and points
-  local_crs = points.crs
 
-  # Set the CRS to 4326 because it is used by Mapillary
-  points.to_crs(crs=4326, inplace=True)
-  
-  # Add a new column to gdf_points that contains the tile coordinates for each point
-  points["tile"] = [mercantile.tile(x, y, zoom) for x, y in zip(points.geometry.x, points.geometry.y)]
-
-  # Group the points by their corresponding tiles
-  groups = points.groupby("tile")
-
-  # Download the tiles and extract the features for each group
-  features = []
-  
-  # To make the process faster the tiles are downloaded using threads
-  with ThreadPoolExecutor(max_workers=10) as executor:
-    futures = []
-
-    for tile, _ in groups:
-      futures.append(executor.submit(get_features_for_tile, tile, access_token))
-    
-    for future in tqdm(as_completed(futures), total=len(futures), desc="Downloading tiles"):
-      result = future.result()
-      features.append(result)
-
-  pd_features = pd.DataFrame(features, columns=["tile", "features"])
-
-  # Compute distances between each feature and all the points in gdf_points
-  feature_points = gpd.GeoDataFrame(
-    [(Point(f["geometry"]["coordinates"]), f) for row in pd_features["features"] for f in row["features"]],
-    columns=["geometry", "feature"],
-    geometry="geometry",
-    crs=4326)
-
-  # Transform from EPSG:4326 (world °) to the local crs in meters that we got when we projected the roads graph in the previous step
-  feature_points.to_crs(local_crs, inplace=True)
-  points.to_crs(local_crs, inplace=True)
-
-  # Create a KDTree (k-dimensional tree) from the "geometry" coordinates of feature_points
-  feature_tree = cKDTree(feature_points["geometry"].apply(lambda p: [p.x, p.y]).tolist())
-  # Use the KDTree to query the nearest neighbors of the points in the "geometry" column of points DataFrame
-  # The query returns the distances and indices of the nearest neighbors
-  # The parameter "k=1" specifies that we want to find the nearest neighbor
-  # The parameter "distance_upper_bound=max_distance" sets a maximum distance for the nearest neighbors
-  distances, indices = feature_tree.query(points["geometry"].apply(lambda p: [p.x, p.y]).tolist(), k=1, distance_upper_bound=max_distance/2)
-
-  # Create a list to store the closest features and distances to each point. If there are no images close then set the value of both to None
-  closest_features = [feature_points.loc[i, "feature"] if np.isfinite(distances[idx]) else None for idx, i in enumerate(indices)]
-  closest_distances = [distances[idx] if np.isfinite(distances[idx]) else None for idx in range(len(distances))]
-
-  # Store the closest feature for each point in the "feature" column of the points DataFrame
-  points["feature"] = closest_features
-
-  # Store the distances as a new column in points
-  points["distance"] = closest_distances
-
-  # Store image id and is_panoramic information as part of the dataframe
-  points["image_id"] = points.apply(lambda row: str(row["feature"]["properties"]["id"]) if row["feature"] else "", axis=1)
-  points["image_id"] = points["image_id"].astype(str)
-  
-  points["is_panoramic"] = points.apply(lambda row: bool(row["feature"]["properties"]["is_pano"]) if row["feature"] else None, axis=1)
-  points["is_panoramic"] = points["is_panoramic"].astype(bool)
-
-  # Add camera angle
-  header = {'Authorization': 'OAuth {}'.format(access_token)}
-
-  points["camera_angle"] = points.apply(lambda row: requests.get(f'https://graph.mapillary.com/{row["image_id"]}?fields=compass_angle',
-                                                                 headers=header).json()["compass_angle"] if row["feature"] else None, axis=1)
-  
-  # Add road angle
-  points = gpd.sjoin_nearest(points, road, how='left', max_distance = 0.2)
-
-  columns_to_drop = ["index_right0", "index_right1", "index_right2", "osmid", "name", "highway", "oneway", "reversed", "length",
-                     "maxspeed", "lanes", "width", "bridge", "access", "tunnel", "index", "ref", "junction", "service"]
-  
-  for column in columns_to_drop:
-    if column in points.columns:
-      points = points.drop(columns=[column])
-
-  # Convert results to geodataframe
-  points["road_index"] = points["road_index"].astype(str)
-  points["tile"] = points["tile"].astype(str)
-
-  # Save the current index as a column
-  points["id"] = points.index
-  points = points[~points.index.duplicated(keep="first")]
-  points = points.reset_index(drop=True)
 
   # Transform the coordinate reference system to EPSG 4326
   points.to_crs(epsg=4326, inplace=True)
